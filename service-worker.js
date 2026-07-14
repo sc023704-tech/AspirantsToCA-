f5e6b37164e1d3522cece3c8f7884e08c82f2410
sc023704-tech <sc023704@gmail.com>
2026-07-14 11:14:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ca-elite-v6';
+const CACHE_NAME = 'ca-elite-v8';
 const ASSETS = ['index.html', 'style.css', 'script.js', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
